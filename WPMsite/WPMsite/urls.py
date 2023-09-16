bdd5fcc3e75fb490408dfb388e78f521a2aecaaa
Zuko1337 <zuko13371@yandex.ru>
2023-09-16 21:02:08 +0300

    Добавил логику и файлы

--- WPMsite/WPMsite/urls.py
+++ WPMsite/WPMsite/urls.py
@@ -1,5 +1,8 @@
-from django.contrib import admin
 from django.urls import path
+from mainapp import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('words_page/', views.words_page, name='words_page'),
+    path('check_word/', views.check_word, name='check_word')
 ]
--- WPMsite/mainapp/views.py
+++ WPMsite/mainapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Домашняя страница</h1>")
+
+
+def check_word(request):
+    input_word = request.GET.get('word')
+    expected_words = ["Привет", "Пока", "Спасибо"]  # Список ожидаемых слов
+
+    if input_word in expected_words:
+        response_data = {'message': 'Слово введено правильно.'}
+    else:
+        response_data = {'message': 'Введено неправильное слово.'}
+
+    return JsonResponse(response_data)
+
+
+def words_page(request):
+    return render(request, 'mainapp/word.html')
+
